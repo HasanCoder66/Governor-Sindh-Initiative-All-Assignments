8fe53cd5dc4211a5065568b9fa778e94cbfa17c3
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-10-16 13:15:31 +0500

    cli calculator done

---
+++ calculator/index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const prompts = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Division", "Multiplicaton"],
+    },
+]);
+console.log(prompts);
+if (prompts.operator === "Addition") {
+    console.log("your value is", prompts.firstNumber + prompts.secondNumber);
+}
+else if (prompts.operator === "Subraction") {
+    console.log("your value is", prompts.firstNumber - prompts.secondNumber);
+}
+else if (prompts.operator === "Multiplicaton") {
+    console.log("your value is", prompts.firstNumber * prompts.secondNumber);
+}
+else if (prompts.operator === "Division") {
+    console.log("your value is", prompts.firstNumber / prompts.secondNumber);
+}
+else {
+    console.log("Please select valid operation name");
+}
