ac0afad24a07c85f097461d31a65ffed86fc6886
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-10-16 15:41:29 +0500

    error in npx command

--- calculator/index.js
+++ calculator/index.js
@@ -1,3 +1,4 @@
+#! usr/bin/env node
 import inquirer from "inquirer";
 const prompts = await inquirer.prompt([
     { message: "Enter first number", type: "number", name: "firstNumber" },
@@ -6,22 +7,28 @@ const prompts = await inquirer.prompt([
         message: "Select one operator to perform operation",
         type: "list",
         name: "operator",
-        choices: ["Addition", "Subtraction", "Division", "Multiplicaton"],
+        choices: ["Addition", "Subtraction", "Division", "Multiplication"],
     },
 ]);
 console.log(prompts);
-if (prompts.operator === "Addition") {
+const operator = prompts.operator.toLowerCase();
+if (operator === "addition") {
     console.log("your value is", prompts.firstNumber + prompts.secondNumber);
 }
-else if (prompts.operator === "Subraction") {
+else if (operator === "subtraction") {
     console.log("your value is", prompts.firstNumber - prompts.secondNumber);
 }
-else if (prompts.operator === "Multiplicaton") {
+else if (operator === "multiplication") {
     console.log("your value is", prompts.firstNumber * prompts.secondNumber);
 }
-else if (prompts.operator === "Division") {
-    console.log("your value is", prompts.firstNumber / prompts.secondNumber);
+else if (operator === "division") {
+    if (prompts.secondNumber === 0) {
+        console.log("Error: Cannot divide by zero!");
+    }
+    else {
+        console.log("your value is", prompts.firstNumber / prompts.secondNumber);
+    }
 }
 else {
-    console.log("Please select valid operation name");
+    console.log("Please select a valid operation name");
 }
